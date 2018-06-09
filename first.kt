d4f3498905b7128e23549e032b885ee543eec5aa
vipin-developer <40068988+vipin-developer@users.noreply.github.com>
2018-06-09 17:02:05 +0530

    Add files via upload

---
+++ first.kt
@@ -0,0 +1,8 @@
+
+fun main(args :Array<String>){
+    println("hellow shashank")
+     var sum= second();
+
+    sum.c=sum.a+sum.b;
+    println("sum of ${sum.a} and ${sum.b} is"+" "+sum.c)
+}
---
+++ second.kt
@@ -0,0 +1,6 @@
+class second {
+    var a: Int=5;
+    var b:Int=10;
+    var c:Int=10;
+
+}
